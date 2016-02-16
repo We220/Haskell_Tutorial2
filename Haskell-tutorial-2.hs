1218ca45dba2d2c6fab6872b6ae7f2ecf0291f3b
We220 <we220@bath.ac.uk>
2016-02-16 22:15:16 +0000

    Did some more exercises

--- Haskell-tutorial-2.hs
+++ Haskell-tutorial-2.hs
@@ -1,118 +1,119 @@
 
 
 
-------------------------- Arithmetic expressions
+  ------------------------- Arithmetic expressions
 
 
-data Aexp = Num Integer
-           | Aexp :+: Aexp
-           | Aexp :*: Aexp
-           | Aexp :-: Aexp
-             deriving (Eq,Show)
+  data Aexp = Num Integer
+             | Var Variable
+             | Aexp :+: Aexp
+             | Aexp :*: Aexp
+             | Aexp :-: Aexp
+               deriving (Eq,Show)
+
+  eval :: Aexp -> Integer
+  eval (Num n)   = n
+  eval (a :+: b) = eval a + eval b
+  eval (a :*: b) = eval a * eval b
+  eval (a :-: b) = eval a - eval b
 
 
-eval :: Aexp -> Integer
-eval (Num n)   = n
-eval (a :+: b) = eval a + eval b
-eval (a :*: b) = eval a * eval b
-eval (a :-: b) = eval a - eval b
+  ------------------------- Exercise 1
+
+  n1 = Num 4 :*: Num (-6)
+
+  n2 = Num 0 :-: (Num 0 :-: Num 10)
+
+  n3 = Num 3 :*: (Num 4 :+: Num 5)
 
 
-------------------------- Exercise 1
 
-n1 = Num 4 :*: Num (-6)
+  ------------------------- Exercise 2
 
-n2 = Num 0 :-: (Num 0 :-: Num 10)
-
-n3 = Num 3 :*: (Num 4 :+: Num 5)
-
-n4 = Num 3 :-: Num 4
-
-
-------------------------- Exercise 2
-
-natural :: Aexp -> Integer
-natural (Num n)   = if n > 0
-	                then n
-	                else 0
-natural (a :+: b) = eval a + eval b
-natural (a :*: b) = eval a * eval b
-natural (a :-: b) = if eval a - eval b < 0
-	                then 0
-	                else eval a - eval b
+  natural :: Aexp -> Integer
+  natural (Num n)   = if n > 0
+                    then n
+                    else 0
+  natural (a :+: b) = eval a + eval b
+  natural (a :*: b) = eval a * eval b
+  natural (a :-: b) = if eval a - eval b < 0
+                    then 0
+                    else eval a - eval b
 
 
-------------------------- State as a list
+  ------------------------- State as a list
 
-type Variable = String
+  type Variable = String
 
-type State = [(Variable,Integer)]
-
-
-st :: State
-st = [("x",3), ("y",5), ("z",0)]
+  type State = [(Variable,Integer)]
 
 
-------------------------- Exercise 3
-
-empty :: State
-empty = [("",2)]
-
-get :: Variable -> State -> Integer
-get v [] = 0
-get v ((x,i):xs) = if v == x
-	               then i
-	               else get v (xs)
-
-remove :: Variable -> State -> State
-remove v (x,i) = (v,i) 
-remove v ((x,i):xs) = if (get v ((x,i):xs) == 0)
-                      then 0
-                      else remove v (x,i)
-
-set :: Variable -> Integer -> State -> State
-set = undefined
+  st :: State
+  st = [("x",3), ("y",5), ("z",0)]
 
 
-------------------------- Exercise 4
+  ------------------------- Exercise 3
 
--- n4 :: Aexp
--- n4 = (Var "x" :+: Num 6) :*: Var "y"
+  empty :: State
+  empty = []
 
-variables :: Aexp -> [Variable]
-variables = undefined
+  get :: Variable -> State -> Integer
+  get _ [] = 0
+  get v ((x,i):xs) | x == v  = i
+                   | otherwise = get v xs
 
-evaluate :: Aexp -> State -> Integer
-evaluate = undefined
+  remove :: Variable -> State -> State
+  remove v [] = []
+  remove v (x:xs) | fst x == v = xs
+                  | otherwise = x : remove v xs
+
+  set :: Variable -> Integer -> State -> State
+  set v x s = (v,x) : remove v s
 
 
-------------------------- State as a function
+  ------------------------- Exercise 4
 
-type ST = Variable -> Integer
+  n4 :: Aexp
+  n4 = (Var "x" :+: Num 6) :*: Var "y"
+
+  variables :: Aexp -> [Variable]
+  variables (Var "n") = ["n"]
+  variables (Num n)   = 
+  variables (a :+: b) = 
+  variables (a :*: b) = 
+  variables (a :-: b) = 
+
+  evaluate :: Aexp -> State -> Integer
+  evaluate = undefined
 
 
-------------------------- Exercise 5
+  ------------------------- State as a function
 
-nil :: ST
-nil v = undefined
+  type ST = Variable -> Integer
 
-one :: ST
-one v | v == "a"  = undefined
-      | otherwise = undefined
 
-two :: ST
-two v = undefined
+  ------------------------- Exercise 5
 
-putCto3 :: ST -> ST
-putCto3 st v = undefined
+  nil :: ST
+  nil v = undefined
 
-three :: ST
-three = putCto3 two
+  one :: ST
+  one v | v == "a"  = undefined
+        | otherwise = undefined
 
-put :: Variable -> Integer -> ST -> ST
-put = undefined
+  two :: ST
+  two v = undefined
 
-evalST :: Aexp -> ST -> Integer
-evalST = undefined
+  putCto3 :: ST -> ST
+  putCto3 st v = undefined
+
+  three :: ST
+  three = putCto3 two
+
+  put :: Variable -> Integer -> ST -> ST
+  put = undefined
+
+  evalST :: Aexp -> ST -> Integer
+  evalST = undefined
 
 
